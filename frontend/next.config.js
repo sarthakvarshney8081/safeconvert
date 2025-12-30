355ac5cf4b649d92d5f18675b800fc084a3ea8f1
SARTHAK VARSHNEY <sarthakvarshney@SARTHAKs-MacBook-Pro.local>
2025-12-30 22:29:21 +0530

    Configure Next.js Proxy to backend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    // Proxy API requests to backend container to keep backend private
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://backend:8000/:path*', // Proxy to Backend Service
+            },
+        ]
+    },
 };
 
 module.exports = nextConfig;
