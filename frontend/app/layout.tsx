b902f9bd67123a91779f7644527814e80c7e2ea2
SARTHAK VARSHNEY <sarthakvarshney@SARTHAKs-MacBook-Pro.local>
2026-01-23 18:56:32 +0530

    feat: Add Google Tag Manager scripts

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -55,6 +55,23 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className} suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TXN79472"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TXN79472');
+          `}
+        </Script>
         {process.env.NEXT_PUBLIC_GA_ID && (
           <>
             <Script
